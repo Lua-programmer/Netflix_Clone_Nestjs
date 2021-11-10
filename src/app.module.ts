f8063265cf4f89abe95694656d13041ce4348613
Lua-programmer <[luanamelissaprogrammer@gmail.com]>
2021-11-10 19:31:40 -0300

    rota post users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,36 @@
-import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { MoviesModule } from './movies/movies.module';
-import { SeriesService } from './series/series.service';
-import { SeriesController } from './series/series.controller';
-import { SeriesModule } from './series/series.module';
+import { Module
+} from '@nestjs/common';
+
+import { UsersModule
+} from './users/users.module';
+
+import { UsersController
+} from './users/users.controller';
+
+import { UsersService
+} from './users/users.service';
+
+import { MoviesModule
+} from './movies/movies.module';
+
+import { MoviesController
+} from './movies/movies.controller';
+
+import { MoviesService
+} from './movies/movies.service';
+
+import { SeriesService
+} from './series/series.service';
+
+import { SeriesController
+} from './series/series.controller';
+
+import { SeriesModule
+} from './series/series.module';
 
 @Module({
   imports: [UsersModule, MoviesModule, SeriesModule],
-  controllers: [SeriesController],
-  providers: [SeriesService],
+  controllers: [SeriesController, UsersController, MoviesController],
+  providers: [SeriesService, UsersService, MoviesService],
 })
 export class AppModule {}
--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
-
+import { PrismaService } from 'src/prisma.service';
 @Module({
   controllers: [MoviesController],
-  providers: [MoviesService]
+  providers: [MoviesService, PrismaService]
 })
 export class MoviesModule {}
